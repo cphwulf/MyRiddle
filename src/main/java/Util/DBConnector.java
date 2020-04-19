8ad48476a95efbaa8201bfc95bb30bd4b618e562
thor <thor@cphbusiness.dk>
2020-04-20 00:54:12 +0200

    riddle

--- src/main/java/Util/DBConnector.java
+++ src/main/java/Util/DBConnector.java
@@ -1,28 +1,34 @@
 package Util;
 import java.sql.Connection;
-import java.sql.SQLDataException;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnector {
 
+    private Connection connection;
     private static DBConnector instance;
 
-    // private constructor
     private DBConnector() {
-        // url
-        // credentials
-        // database
-        
-
-
+        try {
+            String baseurl = "jdbc:mysql://localhost:3306/";
+            String db = "quiz";
+            String timeZ = "serverTimezone=UTC&allowPublicKeyRetrieval=true";
+            String totalUrl = baseurl+db+"?"+timeZ;
+            String user = "root";
+            String password = "root123";
+            connection = DriverManager.getConnection(totalUrl,user,password);
+        } catch (SQLException id) {
+            System.out.println("Wrong " + id.getMessage());
+        }
     }
-
     public static DBConnector getInstance() {
-        if (instance == null) {
+        if (instance == null ) {
             instance = new DBConnector();
         }
         return instance;
     }
-    // static get instans
 
-
+    public Connection getConnection() {
+        return this.connection;
+    }
 }
